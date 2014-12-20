ef7db5bb06209a221b2a1161c8b71b371919866c
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2014-12-20 19:10:10 +0000

    Init travis & gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,15 +48,49 @@ module.exports = function(grunt) {
           useAvailablePort: true,
         }
       }
+    },
+
+    'gh-pages': {
+      options: {
+        branch: 'gh-pages',
+        base: 'build'
+      },
+      deploy: {
+        options: {
+          user: {
+            name: 'Emmanouil Konstantinidis',
+            email: 'manos@iamemmanouil.com'
+          },
+          repo: 'https://' + process.env.GH_TOKEN + '@github.com/ekonstantinidis/ui-color.git',
+          message: 'publish gh-pages (auto)' + getDeployMessage(),
+          silent: true
+        },
+        src: ['**/*']
+      }
     }
 
   });
+
+  function getDeployMessage() {
+    var ret = '\n\n';
+    if (process.env.TRAVIS !== 'true') {
+      ret += 'missing env vars for travis-ci';
+      return ret;
+    }
+    ret += 'branch:       ' + process.env.TRAVIS_BRANCH + '\n';
+    ret += 'SHA:          ' + process.env.TRAVIS_COMMIT + '\n';
+    ret += 'range SHA:    ' + process.env.TRAVIS_COMMIT_RANGE + '\n';
+    ret += 'build id:     ' + process.env.TRAVIS_BUILD_ID  + '\n';
+    ret += 'build number: ' + process.env.TRAVIS_BUILD_NUMBER + '\n';
+    return ret;
+  }
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('default', ['build']);
   grunt.registerTask('build', ['jshint', 'less', 'copy']);
