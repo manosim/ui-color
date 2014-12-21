7d086a9ca35f0b73094eaedd988c6c884c5f390d
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2014-12-21 20:37:29 +0000

    Remove inverted color

--- src/js/controllers/header.js
+++ src/js/controllers/header.js
@@ -7,27 +7,10 @@ app.controller("HeaderCtrl", function(appConfig, $scope, $location) {
         return active;
     };
 
-    function invertColor(hexTripletColor) {
-        var color = hexTripletColor;
-        color = color.substring(1);           // remove #
-        color = parseInt(color, 16);          // convert to integer
-        color = 0xFFFFFF ^ color;             // invert three bytes
-        color = color.toString(16);           // convert to hex
-        color = ("000000" + color).slice(-6); // pad with leading zeros
-        color = "#" + color;                  // prepend #
-        return color;
-    }
-
     $scope.selectedColor = appConfig.themePrimary;
-    $scope.invertedColor = "none";
 
     $scope.$on('ColorChanged', function(event, args) {
-            $scope.selectedColor = args;
-            if (args == appConfig.themePrimary) {
-                $scope.invertedColor = "#FFFFFF";
-            } else {
-                $scope.invertedColor = invertColor(args);
-            }
+        $scope.selectedColor = args;
     });
 
 });
