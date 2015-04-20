5692b48d90f5804b4efe0cc0d4f213c5e853aeb4
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-04-20 22:49:13 +0100

    Button Groups

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,24 @@
 'use strict';
 
 var React = require('react');
+var ReactBootstrap  = require('react-bootstrap');
+
+var Switches = React.createClass({
+    render: function () {
+        return (
+            <ReactBootstrap.ButtonGroup>
+                <ReactBootstrap.Button className='btn btn-success'>HEX</ReactBootstrap.Button>
+                <ReactBootstrap.Button className='btn btn-success'>RGB</ReactBootstrap.Button>
+            </ReactBootstrap.ButtonGroup>
+        );
+    }
+});
 
 var App = React.createClass({
   render: function () {
     return (
       <div className='app'>
-        Hello UIColor!
+        <Switches />
       </div>
     );
   }
