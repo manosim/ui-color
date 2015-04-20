d9cecc3612f7c056121475966cf41aeeee92c4bc
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-04-20 23:47:34 +0100

    Init button group states

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,40 @@ var React = require('react');
 var ReactBootstrap  = require('react-bootstrap');
 
 var Switches = React.createClass({
+    getInitialState: function () {
+        return {type: "hex"};
+    },
+
+    componentDidMount: function (){
+
+    },
+
+    handleClick: function (value) {
+        console.log(this.setState.type);
+        console.log(value);
+
+        this.setState({
+          type : "rgb"
+        });
+
+        // if (this.setState.type == "hex") {
+        //     this.replaceState({
+        //       type : "rgb"
+        //     });
+        // } else {
+        //     this.replaceState({
+        //       type : "hex"
+        //     });
+        // }
+    },
+
     render: function () {
+        var hexClassName = this.state.type === 'hex' ? 'btn btn-success active' : 'btn btn-success ';
+        var rgbClassName = this.state.type === 'rgb' ? 'btn btn-success active' : 'btn btn-success ';
         return (
             <ReactBootstrap.ButtonGroup>
-                <ReactBootstrap.Button className='btn btn-success'>HEX</ReactBootstrap.Button>
-                <ReactBootstrap.Button className='btn btn-success'>RGB</ReactBootstrap.Button>
+                <ReactBootstrap.Button onClick={this.handleClick} className={hexClassName}>HEX</ReactBootstrap.Button>
+                <ReactBootstrap.Button onClick={this.handleClick} className={rgbClassName}>RGB</ReactBootstrap.Button>
             </ReactBootstrap.ButtonGroup>
         );
     }
