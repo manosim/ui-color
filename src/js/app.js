fa8892e58e4a2c9a0b51dd51c4f105698a80dc92
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-04-20 22:26:58 +0100

    Init React

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-console.log("Hello React");
+'use strict';
+
+var React = require('react');
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div className='app'>
+        Hello UIColor!
+      </div>
+    );
+  }
+});
+
+React.render(
+  <App />,
+  document.getElementById('app')
+);
