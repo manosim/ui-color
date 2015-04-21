6a6eefc39b1a6e64b8b1362f8aa2b7e6fbea3c13
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-04-21 20:56:15 +0100

    Init HexInput

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,50 @@
 var React = require('react');
 var ReactBootstrap  = require('react-bootstrap');
 
+const HexInput = React.createClass({
+  getInitialState() {
+    return {
+      hexValue: "#0072bc"
+    };
+  },
+
+  validationState() {
+    let length = this.state.hexValue.length;
+    if (length < 3 || length > 7) { return 'error'; }
+  },
+
+  handleChange() {
+    // This could also be done using ReactLink:
+    // http://facebook.github.io/react/docs/two-way-binding-helpers.html
+    this.setState({
+      hexValue: this.refs.input.getValue()
+    });
+  },
+
+  render() {
+    return (
+      <ReactBootstrap.Grid>
+          <ReactBootstrap.Row className='converter hex-to-uicolor'>
+            <ReactBootstrap.Col xs={12} mdOffset={3} md={6}>
+              <ReactBootstrap.Input
+                type='text'
+                value={this.state.hexValue}
+                placeholder='eg. 0072BC'
+                help='Enter a valid HEX without the hashtag #.'
+                bsStyle={this.validationState()}
+                hasFeedback
+                ref='input'
+                groupClassName='group-class'
+                wrapperClassName='wrapper-class'
+                labelClassName='label-class'
+                onChange={this.handleChange} />
+            </ReactBootstrap.Col>
+          </ReactBootstrap.Row>
+      </ReactBootstrap.Grid>
+    );
+  }
+});
+
 var Switches = React.createClass({
     getInitialState: function () {
         return {type: "hex"};
@@ -39,8 +83,9 @@ var Switches = React.createClass({
 var App = React.createClass({
   render: function () {
     return (
-      <div className='app'>
+      <div className="app">
         <Switches />
+        <HexInput />
       </div>
     );
   }
