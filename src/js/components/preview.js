8f086f2fd3e51813dcfb8ba742f77207c243bb20
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-04-23 23:42:05 +0100

    Disable preview if no valid hex

--- src/js/components/preview.js
+++ src/js/components/preview.js
@@ -3,20 +3,26 @@ var React = require('react');
 var PreviewColor = React.createClass({
   getInitialState: function() {
     return {
-      color: this.props.color || '#0072bc'
-    }
+      color: this.props.color || '#f7f7f9'
+    };
   },
 
   componentWillReceiveProps: function (nextProps) {
+    var newColor;
+    if (nextProps.color) {
+       newColor = nextProps.color;
+    } else {
+       newColor = 'f7f7f9';
+    }
     this.setState({
-      color: "#" + nextProps.color
+      color: "#" + newColor
     });
   },
 
   render: function() {
     var previewStyle = {
       backgroundColor: this.state.color
-    }
+    };
     return (
       <div className="form-group color-demo" style={previewStyle} />
     );
