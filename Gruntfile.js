972733ac218d61d52839c88d488080f0973b16e0
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-04-25 01:12:02 +0100

    Update grunt watch name

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
         files: 'src/less/*',
         tasks: ['less'],
       },
-      src: {
+      images: {
         files: 'src/images/*',
         tasks: ['copy'],
       },
