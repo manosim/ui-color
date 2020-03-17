6f4f23147c1c02f7aa846d27e1748fcc80ced7d0
Emmanouil Konstantinidis <hello@manos.im>
2020-03-17 22:19:42 +0000

    Fix filename

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { Layout } from '../components/Layout';
-import SEO from '../components/seo';
+import SEO from '../components/Seo';
 
 const NotFoundPage = () => (
   <Layout>
